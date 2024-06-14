998082faf254207af1bbc6b3b98c4e4f553c1df7
Yevheniy Dolinskiy <zkorvoz02@gmail.com>
2024-06-14 16:28:11 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,28 +1,29 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class Validator(ABC):
-    def __set_name__(self, owner, name):
+    def __set_name__(self, owner: Any, name: str) -> None:
         self.protected_name = "_" + name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: Any, owner: Any) -> Any:
         return getattr(instance, self.protected_name)
 
-    def __set__(self, instance, value):
+    def __set__(self, instance: Any, value: Any) -> None:
         self.validate(value)
         setattr(instance, self.protected_name, value)
 
     @abstractmethod
-    def validate(self, value):
+    def validate(self, value: Any) -> None:
         pass
 
 
 class Number(Validator):
-    def __init__(self, min_value, max_value):
+    def __init__(self, min_value: int, max_value: int) -> None:
         self.min_value = min_value
         self.max_value = max_value
 
-    def validate(self, value):
+    def validate(self, value: Any) -> None:
         if not isinstance(value, int):
             raise TypeError("Quantity should be integer.")
         if not (self.min_value <= value <= self.max_value):
@@ -33,10 +34,10 @@ class Number(Validator):
 
 
 class OneOf(Validator):
-    def __init__(self, *options):
+    def __init__(self, *options: Any) -> None:
         self.options = options
 
-    def validate(self, value):
+    def validate(self, value: Any) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
 
